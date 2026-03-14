4a9340e55dc9e11213b61c1105d6e9f67a751aa8
Jon Steinich <jsteinich@gmail.com>
2026-03-14 16:07:47 -0500

    use correct property name

--- tools/generate-function-bindings/eslint.config.mjs
+++ tools/generate-function-bindings/eslint.config.mjs
@@ -48,7 +48,7 @@ export default [
   {
     languageOptions: {
       parserOptions: {
-        tsconfigOverride: import.meta.dirname,
+        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
