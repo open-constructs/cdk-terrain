b7b6a549013be94b412fb2b2f26dd1b34ab96cc8
Simon Heather <simon.heather@yulife.com>
2026-06-02 21:12:50 +0100

    chore(cli): decouple cdktn bin entry point from build output

--- packages/cdktn-cli/build.ts
+++ packages/cdktn-cli/build.ts
@@ -83,7 +83,7 @@ const config: esbuild.BuildOptions = {
     {
       name: "rebuild-log",
       setup({ onStart, onEnd }) {
-        let t;
+        let t: number;
         onStart(() => {
           t = Date.now();
         });
@@ -102,7 +102,6 @@ const config: esbuild.BuildOptions = {
 (async () => {
   console.log("Building…");
   await esbuild.build(config);
-  fs.copySync("src/bin/cdktn", "./bundle/bin/cdktn");
   fs.copySync("../@cdktn/cli-core/templates", "./bundle/templates");
 
   if (enableWatch) {
