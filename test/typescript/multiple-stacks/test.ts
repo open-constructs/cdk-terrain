99f2e80c99a75db0306dcc1e1e6fad96b17c1d4a
Simon Heather <simon.heather@yulife.com>
2026-06-26 13:56:39 +0100

    Revert typescript/multiple-stacks test snapshot

--- test/typescript/multiple-stacks/test.ts
+++ test/typescript/multiple-stacks/test.ts
@@ -49,21 +49,22 @@ describe("multiple stacks", () => {
 
     onPosix("list posix", () => {
       expect(driver.list()).toMatchInlineSnapshot(`
-       "Stack name  Path
-       first       cdktf.out/stacks/first
-       second      cdktf.out/stacks/second
-       "
+        "
+        Stack name                      Path
+        first                           cdktf.out/stacks/first
+        second                          cdktf.out/stacks/second
+        "
       `);
     });
 
     onWindows("list windows", () => {
       expect(driver.list()).toMatchInlineSnapshot(`
-             "
-             Stack name                      Path
-             first                           cdktf.out\\stacks\\first
-             second                          cdktf.out\\stacks\\second
-             "
-          `);
+      "
+      Stack name                      Path
+      first                           cdktf.out\\stacks\\first
+      second                          cdktf.out\\stacks\\second
+      "
+    `);
     });
 
     // completions for stacks relies on a manifest.json being present
