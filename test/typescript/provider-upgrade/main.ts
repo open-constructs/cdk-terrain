1945b7747d263fdf9d0ae016d10d9ef33ab23ecc
Jon Steinich <jsteinich@gmail.com>
2026-07-18 17:32:28 -0500

    fix typescript provider upgrade

--- test/typescript/provider-upgrade/main.ts
+++ test/typescript/provider-upgrade/main.ts
@@ -2,13 +2,13 @@
 // SPDX-License-Identifier: MPL-2.0
 import { Construct } from "constructs";
 import { App, TerraformStack, Testing } from "cdktn";
-import { AzureadProvider } from "@cdktn/provider-azuread";
+import { provider } from '@cdktn/provider-azuread'
 
 export class HelloTerra extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new AzureadProvider(this, "provider", {});
+    new provider.AzureadProvider(this, "provider", {});
   }
 }
 
